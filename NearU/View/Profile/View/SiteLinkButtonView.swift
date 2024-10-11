a7db95066df2d403ccd7c7ce831d9d6413e2a20e
Ukyo <ukyou20011121@gmail.com>
2024-10-11 16:06:53 +0900

    add: 記事のリンクボタンファイルの追加

---
+++ NearU/View/Profile/View/SiteLinkButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  SiteLinkButtonView.swift
+//  NearU
+//
+//  Created by 谷口右京 on 2024/10/07.
+//
+
+import SwiftUI
+
+struct SiteLinkButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SiteLinkButtonView()
+}
