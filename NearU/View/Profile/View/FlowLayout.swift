47c598c2a2fa6ec00a3a33027c32a1fd1c7e9ab1
vivy_phtela <tubasa.0405@icloud.com>
2024-10-15 19:47:39 +0900

    delete: コメントアウトの削除

---
+++ NearU/View/Profile/View/FlowLayout.swift
@@ -0,0 +1,18 @@
+//
+//  FlowLayout.swift
+//  NearU
+//
+//  Created by Tsubasa Watanabe on 2024/10/15.
+//
+
+import SwiftUI
+
+struct FlowLayout: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlowLayout()
+}
---
+++ NearU/View/Profile/View/TagsView.swift
@@ -0,0 +1,18 @@
+//
+//  TagsView.swift
+//  NearU
+//
+//  Created by Tsubasa Watanabe on 2024/10/15.
+//
+
+import SwiftUI
+
+struct TagsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TagsView()
+}
