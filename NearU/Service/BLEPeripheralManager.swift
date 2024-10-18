c8a52f17bc114eda750cefd033c697dc19f6291a
Wataru Takahashi <wataru1355@icloud.com>
2024-10-18 18:26:58 +0900

    fix: GATT通信ではなくスキャンアドバタイズ方式でのBLE通信に変更

--- NearU/Service/BLEPeripheralManager.swift
+++ NearU/Service/BLEPeripheralManager.swift
@@ -9,26 +9,22 @@ import Foundation
 import FirebaseAuth
 import FirebaseFirestoreSwift
 import Firebase
+import Combine
 
 // デバイスがペリフェラルとしてデータを提供するためのクラス
 class BLEPeripheralManager: NSObject, ObservableObject, CBPeripheralManagerDelegate {
     static let shared = BLEPeripheralManager() // シングルトンインスタンス
-    // サービスやキャラクタリスティックの作成を管理するインスタンス変数
     var peripheralManager: CBPeripheralManager!
-    var userId: String?
-    var advertisingTimer: Timer? // 定期的な出力用のタイマーを追加
+    var documentId: String?
+    var advertisingTimer: Timer?
 
-    let serviceUUID = CBUUID(string: "12345678-1234-1234-1234-1234567890ab")
-    let characteristicUUID = CBUUID(string: "87654321-4321-4321-4321-9876543210ba")
-
-    private override init() { // 外部プログラムからインスタンス生成ができないようにprivate
+    private override init() {
         super.init()
-        // インスタンスを作成し，デリゲードを自身に設定
         peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
     }
 
     func configure(with user: User) {
-        self.userId = user.id
+        self.documentId = user.id
     }
 
     func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
@@ -42,53 +38,20 @@ class BLEPeripheralManager: NSObject, ObservableObject, CBPeripheralManagerDeleg
         }
     }
 
-    // BLEペリフェラルが書き込みリクエストを受け取ったときに呼び出される関数
-    // userIdを受け取りRealmに保存
-    func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveWrite requests: [CBATTRequest]) {
-        // 受け取ったリクエストを処理
-        for request in requests {
-            // リクエストからデータを取り出し，それをString型に変換
-            if let userIdData = request.value, let receivedUserId = String(data: userIdData, encoding: .utf8) {
-                // RealmManager.shared.storeData(receivedUserId) をバックグラウンドスレッドで呼び出す
-                DispatchQueue.main.async {
-                    RealmManager.shared.storeData(receivedUserId)
-                }
-                print("Received userId: \(receivedUserId)")
-            }
-            // centralmanagerに対して書き込みが成功したことを通知
-            peripheralManager.respond(to: request, withResult: .success)
-        }
-    }
-
     func startAdvertising() {
         let isOnBluetooth = UserDefaults.standard.bool(forKey: "isOnBluetooth")
         if isOnBluetooth {
-            // CBMutableCharacteristicを使って特性を作成
-            let characteristic = CBMutableCharacteristic(
-                type: characteristicUUID,
-                properties: [.write], // 書き込み可能
-                value: nil, // 初期値
-                permissions: [.writeable] // アクセス権，書き込み可能
-            )
-            // CBMutableServiceを使ってサービスを作成
-            let service = CBMutableService(type: serviceUUID, primary: true)
-            // サービスに作成した特性を追加
-            service.characteristics = [characteristic]
-            // ペリフェラルが提供するサービスとして登録
-            peripheralManager.add(service)
-            // startAdvertisingメソッドでBLEアドバタイズを開始
-            peripheralManager.startAdvertising([CBAdvertisementDataServiceUUIDsKey: [service.uuid],
-                                                   CBAdvertisementDataLocalNameKey: "Chonnect"])
+            guard let documentId = self.documentId else { return }
+
+            // アドバタイズデータに常にuserIdをLocalNameとして設定
+            let advertisementData: [String: Any] = [
+                CBAdvertisementDataServiceUUIDsKey: [CBUUID(string: "12345678-1234-1234-1234-1234567890ab")],
+                CBAdvertisementDataLocalNameKey: documentId
+            ]
+
+            peripheralManager.startAdvertising(advertisementData)
             print("start Advertising")
 
-            // アドバタイズが開始されたかを確認
-            if peripheralManager.isAdvertising {
-                print("アドバタイズが正常に開始されました")
-            } else {
-                print("アドバタイズが開始されていません")
-            }
-
-            // バックグラウンドでもアドバタイズが続いているかを確認するためのタイマー
             advertisingTimer = Timer.scheduledTimer(withTimeInterval: 5.0, repeats: true) { _ in
                 if self.peripheralManager.isAdvertising {
                     print("アドバタイズが継続中")
@@ -111,4 +74,9 @@ class BLEPeripheralManager: NSObject, ObservableObject, CBPeripheralManagerDeleg
         self.peripheralManager.delegate = nil
     }
 }
+
+
+
+
+
 
