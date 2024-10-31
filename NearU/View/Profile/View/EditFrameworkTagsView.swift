15d1597d1e07131fdbf02b3910ea0cd2d667c31b
vivy_phtela <tubasa.0405@icloud.com>
2024-10-31 17:45:41 +0900

    add: タグを編集するビューを作成

---
+++ NearU/View/Profile/View/EditFrameworkTagsView.swift
@@ -0,0 +1,48 @@
+//
+//  EditTagsView.swift
+//  NearU
+//
+//  Created by Tsubasa Watanabe on 2024/10/16.
+//
+import SwiftUI
+
+struct EditFrameworkTagsView: View {
+    @State private var FrameworkTags: [String] = ["React","Next.js","Vue","Nuxt.js","Angular","Express","Node.js","Django","Flask","Laravel","CakePHP","Flutter","Rails","Remix","Tailwind CSS","Spring"]
+    @Binding var selectedFrameworkTags: [String]
+    let userId: String
+
+    var body: some View {
+        VStack {
+            if FrameworkTags.isEmpty {
+            } else {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 7) {
+                        ForEach(FrameworkTags, id: \.self) { tag in
+                            Text(tag)
+                                .padding(.vertical, 5)
+                                .padding(.horizontal, 12)
+                                .background(self.selectedFrameworkTags.contains(tag) ? Color.blue : Color(.systemGroupedBackground))
+                                .foregroundColor(self.selectedFrameworkTags.contains(tag) ? .white : .black)
+                                .cornerRadius(15)
+                                .onTapGesture {
+                                    toggleTagSelection(tag: tag)
+                                }
+                        }
+                    }
+                    .padding(5)
+                }
+                .frame(height: 35)
+            }
+        }
+        .padding(.bottom, 15)
+    }
+
+    // タグの選択をトグルし、Firestoreに保存する関数
+    private func toggleTagSelection(tag: String) {
+        if let index = selectedFrameworkTags.firstIndex(of: tag) {
+            selectedFrameworkTags.remove(at: index)
+        } else {
+            selectedFrameworkTags.append(tag)
+        }
+    }
+}
