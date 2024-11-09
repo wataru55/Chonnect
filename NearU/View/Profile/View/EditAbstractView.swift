af31182110997620b35d346cc4b2f8989617e8b2
Ukyo <ukyou20011121@gmail.com>
2024-11-09 13:44:08 +0900

    update: Tab文字抜き

---
+++ NearU/View/Profile/View/EditAbstractView.swift
@@ -0,0 +1,18 @@
+//
+//  EditAbstractView.swift
+//  NearU
+//
+//  Created by 谷口右京 on 2024/11/08.
+//
+
+import SwiftUI
+
+struct EditAbstractView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditAbstractView()
+}
