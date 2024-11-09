1461007281f5df0db29b30eddf7b588286382382
Wataru Takahashi <wataru1355@icloud.com>
2024-11-09 14:36:17 +0900

    fix: 軽微な修正

--- NearU/View/Profile/ViewModel/CurrentUserProfileViewModel.swift
+++ NearU/View/Profile/ViewModel/CurrentUserProfileViewModel.swift
@@ -21,7 +21,7 @@ class CurrentUserProfileViewModel: ObservableObject {
 
     @Published var selectedLanguageTags: [String] = []
     @Published var selectedFrameworkTags: [String] = []
-    
+
     @Published var abstractUrls: [String] = []
 
     @Published var username = ""
@@ -84,7 +84,7 @@ class CurrentUserProfileViewModel: ObservableObject {
     func updateLanguageTags() async throws {
         try await UserService.saveLanguageTags(userId: user.id, selectedTags: selectedLanguageTags)
     }
-    
+
     @MainActor
     func loadFrameworkTags() async throws {
         do {
@@ -94,11 +94,11 @@ class CurrentUserProfileViewModel: ObservableObject {
             print("Failed to fetch tags: \(error)")
         }
     }
-    
+
     func updateFrameworkTags() async throws {
-                try await UserService.saveFrameworkTags(userId: user.id, selectedTags: selectedFrameworkTags)
+            try await UserService.saveFrameworkTags(userId: user.id, selectedTags: selectedFrameworkTags)
     }
-    
+
     @MainActor
     func loadAbstractLinks() async {
         do {
