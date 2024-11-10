0cb09cedfecd4472b23ea64f3576ce295c011144
Wataru Takahashi <wataru1355@icloud.com>
2024-11-11 02:55:31 +0900

    add: SNSのリンクを保存する関数を追加

--- NearU/Service/UserService.swift
+++ NearU/Service/UserService.swift
@@ -42,6 +42,17 @@ struct UserService {
         return connectedUsers
     }
 
+    static func saveSNSLink(serviceName: String, url: String) async throws{
+        guard let documentId = AuthService.shared.currentUser?.id else { return }
+
+        let data = ["snsLinks.\(serviceName)": url]
+        do {
+            try await Firestore.firestore().collection("users").document(documentId).updateData(data)
+        } catch {
+            throw error
+        }
+    }
+
     static func seveArticleLink(userId: String, url: String) async throws {
         let data = ["abstract_url": url]
         do {
