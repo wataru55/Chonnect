021934564e9caf609bfbafd5313b8e4729967e75
Wataru Takahashi <wataru1355@icloud.com>
2024-11-10 23:11:31 +0900

    add: 記事を編集する画面を作成

--- NearU/View/Profile/View/SiteLinkButtonView.swift
+++ NearU/View/Profile/View/SiteLinkButtonView.swift
@@ -9,52 +9,81 @@ import SwiftUI
 import OpenGraph
 
 struct SiteLinkButtonView: View {
+    @EnvironmentObject var viewModel: ArticleLinksViewModel
+    @State private var isShowAlert: Bool = false
     let ogpData: OpenGraphData
     var showDeleteButton: Bool
+    var isOpenURL: Bool = false
 
     var body: some View {
-        Button {
-            if let url = URL(string: ogpData.url) {
-                UIApplication.shared.open(url)
-            }
-        }label: {
-            VStack(alignment: .leading) {
-                if let data = ogpData.openGraph {
-                    // メタデータが取得できた場合のリンクプレビュー
-                    VStack(alignment: .leading) {
-                        if let imageUrl = data[.image], let url = URL(string: imageUrl) {
-                            AsyncImage(url: url) { image in
-                                image
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(height: 100)
-                                    .cornerRadius(10)
-                            } placeholder: {
-                                Color.gray.frame(height: 100).cornerRadius(10)
+        ZStack(alignment: .topTrailing) {
+            Button {
+                if let url = URL(string: ogpData.url) {
+                    UIApplication.shared.open(url)
+                }
+            } label: {
+                VStack(alignment: .leading) {
+                    if let data = ogpData.openGraph {
+                        // メタデータが取得できた場合のリンクプレビュー
+                        VStack(alignment: .leading) {
+                            if let imageUrl = data[.image], let url = URL(string: imageUrl) {
+                                AsyncImage(url: url) { image in
+                                    image
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(height: 100)
+                                        .cornerRadius(10)
+                                } placeholder: {
+                                    Color.gray.frame(height: 100).cornerRadius(10)
+                                }
+                            }
+                            // タイトルがない場合はurlを表示
+                            if let title = data[.title], !title.isEmpty {
+                                Text(title)
+                                    .font(.headline)
+                                    .lineLimit(1)
+                            } else {
+                                Text(ogpData.url.count > 35 ? String(ogpData.url.prefix(35)) + "..." : ogpData.url)
+                                    .font(.headline)
+                                    .lineLimit(1)
+                                    .padding(.bottom, 2)
+                            }
+                            //.padding(.leading, 15)
+                        }// Vstack
+                        .padding()
+                        .frame(width: 350, height: 150, alignment: .leading)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .shadow(radius: 5)
+                    } //vstack
+                } //vstack
+            } //button
+            .buttonStyle(PlainButtonStyle())
+            .disabled(!isOpenURL)
+
+            if showDeleteButton {
+                Button {
+                    isShowAlert.toggle()
+                } label: {
+                    Image(systemName: "trash")
+                        .foregroundStyle(.black)
+                        .padding()
+                }
+                .alert("確認", isPresented: $isShowAlert) {
+                    Button("削除", role: .destructive) {
+                        Task {
+                            await viewModel.removeArticle(url: ogpData.url)
+                            await viewModel.fetchArticleUrls()
+                            await MainActor.run {
+                                isShowAlert.toggle()
                             }
                         }
-                        // タイトルがない場合はurlを表示
-                        if let title = data[.title], !title.isEmpty {
-                            Text(title)
-                                .font(.headline)
-                                .lineLimit(1)
-                        } else {
-                            Text(ogpData.url.count > 35 ? String(ogpData.url.prefix(35)) + "..." : ogpData.url)
-                                .font(.headline)
-                                .lineLimit(1)
-                                .padding(.bottom, 2)
-                        }
-                        //.padding(.leading, 15)
-                    }// Vstack
-                    .padding()
-                    .frame(width: 350, height: 150, alignment: .leading)
-                    .background(Color.white)
-                    .cornerRadius(10)
-                    .shadow(radius: 5)
-                } //vstack
-            } //vstack
-        } //button
-        .buttonStyle(PlainButtonStyle())
+                    }
+                } message: {
+                    Text("このリンクを削除しますか？")
+                }
+            }
+        } //zstack
     }//body
 } //view
 
