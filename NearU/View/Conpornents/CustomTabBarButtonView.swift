c08def05484d1b6d70c181e654d2efd78bba7ffe
Wataru Takahashi <wataru1355@icloud.com>
2024-11-13 22:54:03 +0900

    add: 再利用できるようにCustomTabBarButtonViewを構造体として作成

--- NearU/View/Conpornents/CustomTabBarButtonView.swift
+++ NearU/View/Conpornents/CustomTabBarButtonView.swift
@@ -8,11 +8,36 @@
 import SwiftUI
 
 struct CustomTabBarButtonView: View {
+    @Binding var selected: Int
+    private var title: String
+    var tag: Int
+
+    init(selected: Binding<Int>, title: String, tag: Int) {
+        self._selected = selected
+        self.title = title
+        self.tag = tag
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            selected = tag
+
+        } label: {
+            VStack(spacing: 0) {
+                Text(title)
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.gray)
+                    .padding(10)
+                Rectangle()
+                    .frame(height: 2)
+                    .foregroundColor(Color(.systemMint))
+                    .opacity(selected != tag ? 0.0 : 1.0)
+            }
+        }
     }
 }
 
 #Preview {
-    CustomTabBarButtonView()
+    CustomTabBarButtonView(selected: .constant(0), title: "すれちがい履歴", tag: 0)
 }
