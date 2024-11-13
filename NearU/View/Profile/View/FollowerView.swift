316bdca16ffcc10f4d2bb6064b2ec5af5c1b0a7a
Wataru Takahashi <wataru1355@icloud.com>
2024-11-14 03:08:16 +0900

    add: フォロワー一覧を表示するビューを作成

--- NearU/View/Profile/View/FollowerView.swift
+++ NearU/View/Profile/View/FollowerView.swift
@@ -8,11 +8,55 @@
 import SwiftUI
 
 struct FollowerView: View {
+    @EnvironmentObject var viewModel: FollowerViewModel
+
+    var currentUser: User
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.vertical, showsIndicators: false) {
+            LazyVStack(spacing: 16) {
+                if viewModel.followers.isEmpty {
+                    Text("フォローされているユーザーがいません")
+                        .font(.footnote)
+                        .fontWeight(.bold)
+                        .foregroundColor(.gray)
+                        .padding()
+                } else {
+                    ForEach(viewModel.followers, id: \.self) { follower in
+                        NavigationLink(value: follower) {
+                            HStack {
+                                CircleImageView(user: follower.user, size: .xsmall, borderColor: .clear)
+                                VStack (alignment: .leading){
+                                    Text(follower.user.username)
+                                        .fontWeight(.bold)
+                                        .foregroundStyle(Color.primary)
+
+                                    if let fullname = follower.user.fullname { //fullnameがnilじゃないなら
+                                        Text(fullname)
+                                            .foregroundStyle(Color.primary)
+                                    }
+
+                                }//vstack
+                                .font(.footnote)
+
+                                Spacer()
+                            }//hstack
+                            .foregroundStyle(.black) //navigationlinkのデフォルトカラーを青から黒に
+                            .padding(.horizontal)
+                        } //navigationlink
+                    } //foreach
+                }
+            } //lazyvstack
+            .padding(.top, 8)
+
+        } //scrollview
+        .refreshable {
+            print("refresh")
+        }
     }
 }
 
 #Preview {
-    FollowerView()
+    FollowerView(currentUser: User.MOCK_USERS[0])
+        .environmentObject(FollowerViewModel())
 }
--- NearU/View/Profile/ViewModel/FollowerViewModel.swift
+++ NearU/View/Profile/ViewModel/FollowerViewModel.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+import Combine
+import Firebase
+
+class FollowerViewModel: ObservableObject {
+    @Published var followers: [UserHistoryRecord] = []
+    private var listener: ListenerRegistration?
+
+    init() {
+        listenForUpdates()
+
+        Task {
+            await loadFollowers()
+        }
+    }
+
+    func loadFollowers() async {
+        do {
+            let users = try await UserService.fetchFollowers()
+
+            await MainActor.run {
+                self.followers = users
+            }
+        } catch {
+            print("Error fetching followers: \(error)")
+        }
+    }
+
+    func listenForUpdates() {
+        guard let documentId = AuthService.shared.currentUser?.id else { return }
+        listener = Firestore.firestore().collection("users").document(documentId).collection("followers")
+            .addSnapshotListener { [weak self] querySnapshot, error in
+                guard let self = self else { return }
+                if let error = error {
+                    print("Error listening for updates: \(error)")
+                    return
+                }
+                guard let _ = querySnapshot else {
+                    print("QuerySnapshot data was empty.")
+                    return
+                }
+                Task {
+                    await self.loadFollowers()
+                }
+            }
+    }
+
+    deinit {
+        listener?.remove()
+    }
+}
