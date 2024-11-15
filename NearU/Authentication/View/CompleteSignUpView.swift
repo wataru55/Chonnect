c0ed0338e0b7699c38ae8681d93b61918238433e
vivy_phtela <tubasa.0405@icloud.com>
2024-11-15 22:50:05 +0900

    fix: 改行を追加

--- NearU/Authentication/View/CompleteSignUpView.swift
+++ NearU/Authentication/View/CompleteSignUpView.swift
@@ -13,7 +13,7 @@ struct CompleteSignUpView: View {
 
     var body: some View {
         VStack (spacing: 10) {
-            Text("Welcome to Chonnect! \(viewModel.username)")
+            Text("Welcome to Chonnect! \n\(viewModel.username)")
                 .multilineTextAlignment(.center)
                 .font(.system(size: 30, weight: .bold))
                 .fontWeight(.bold)
