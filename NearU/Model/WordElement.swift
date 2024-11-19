eb0426e2835b450477a5ea031bc622a8a0aa215a
Wataru Takahashi <wataru1355@icloud.com>
2024-11-19 22:50:19 +0900

    add: タグのデータモデルを作成

---
+++ NearU/Model/WordElement.swift
@@ -0,0 +1,14 @@
+//
+//  WordElement.swift
+//  NearU
+//
+//  Created by  髙橋和 on 2024/11/19.
+//
+
+import Foundation
+
+struct WordElement: Codable, Hashable {
+    let name: String
+    let skill: Int
+    let interest: Int
+}
