12dd8f83de5c5bcbddaea06c5ffdb632047ee96d
Wataru Takahashi <wataru1355@icloud.com>
2024-11-21 23:53:54 +0900

    add: tag関連のfuncをまとめるための構造体を作成

---
+++ NearU/Service/TagsService.swift
@@ -0,0 +1,32 @@
+//
+//  TagsService.swift
+//  NearU
+//
+//  Created by  髙橋和 on 2024/11/21.
+//
+
+import SwiftUI
+import Firebase
+
+struct TagsService {
+
+    static func addTags(tagData: WordElement) async throws {
+        guard let documentId = AuthService.shared.currentUser?.id else { return }
+        let ref = Firestore.firestore().collection("users").document(documentId).collection("electedTags")
+        let id = tagData.id.uuidString
+
+        let data: [String: String] = [
+            "id": id,
+            "name": tagData.name,
+            "skill": tagData.skill,
+            "interest": tagData.interest
+        ]
+
+        do {
+            try await ref.document(id).setData(data)
+        } catch {
+            throw error
+        }
+
+    }
+}
---
+++ NearU/View/Profile/View/CurrentUser/ViewModel/EditSkillTagsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  EditSkillTagsViewModel.swift
+//  NearU
+//
+//  Created by  髙橋和 on 2024/11/21.
+//
+
+import Foundation
