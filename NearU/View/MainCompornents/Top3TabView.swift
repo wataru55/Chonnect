bf23aabe2ca54e82a9c0d6f4ec90332fb0487e5b
Wataru Takahashi <wataru1355@icloud.com>
2024-11-24 18:31:04 +0900

    fix: 見た目の修正

--- NearU/View/MainCompornents/Top3TabView.swift
+++ NearU/View/MainCompornents/Top3TabView.swift
@@ -7,71 +7,83 @@
 
 import SwiftUI
 
-struct Top3TabView: View {
-    var body: some View {
-        HStack {
-            HStack(spacing: -10) {
-                Image("Python")
-                    .resizable()
-                    .scaledToFit()
-                    .padding()
-                    .clipShape(Circle())
-                    .zIndex(2)
-                    .background (
-                        Circle().fill()
-                            .foregroundStyle(.white)
-                            .shadow(color: .gray, radius: 2, x: 0, y: 2)
-                    )
-                    .overlay(alignment: .top) {
-                        Image(systemName: "crown.fill")
-                            .font(.footnote)
-                            .foregroundStyle(.yellow)
-                            .offset(y: -10)
-                    }
+enum Ranking: Int {
+    case first = 0
+    case second = 1
+    case third = 2
 
-                Image("Swift")
-                    .resizable()
-                    .scaledToFit()
-                    .padding()
-                    .clipShape(Circle())
-                    .zIndex(1)
-                    .background (
-                        Circle().fill()
-                            .foregroundStyle(.white)
-                            .shadow(color: .gray, radius: 2, x: 0, y: 2)
-                    )
-                    .overlay(alignment: .top) {
-                        Image(systemName: "crown.fill")
-                            .font(.footnote)
-                            .foregroundStyle(.gray)
-                            .offset(y: -10)
-                    }
+    func color() -> Color {
+        switch self {
+        case .first:
+            return .yellow
+        case .second:
+            return .gray
+        case .third:
+            return .brown
+        }
+    }
 
-                Image("Ruby")
-                    .resizable()
-                    .scaledToFit()
-                    .padding()
-                    .clipShape(Circle())
-                    .background (
-                        Circle().fill()
-                            .foregroundStyle(.white)
-                            .shadow(color: .gray, radius: 2, x: 0, y: 2)
-                    )
-                    .overlay(alignment: .top) {
-                        Image(systemName: "crown.fill")
-                            .font(.footnote)
-                            .foregroundStyle(.brown)
-                            .offset(y: -10)
-                    }
-            }
-            .frame(height: 60)
-
-            Text("... 一覧を表示")
-                .foregroundStyle(.gray)
+    func index() -> Int {
+        switch self {
+        case .first:
+            return 3
+        case .second:
+            return 2
+        case .third:
+            return 1
         }
     }
 }
 
-#Preview {
-    Top3TabView()
+struct Top3TabView: View {
+    let tags: [WordElement]
+
+    var body: some View {
+        HStack {
+            HStack(spacing: -5) {
+                ForEach(Array(tags.prefix(3).enumerated()), id: \.offset) { index, item in
+                    Image("\(item.name)")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 15) // 画像の高さを20に設定
+                        .padding(10) // 上下に5のパディング
+                        .clipShape(Circle())
+                        .zIndex(Double(Ranking(rawValue: index)?.index() ?? 0)) // 前面に表示
+                        .background(
+                            Circle()
+                                .foregroundStyle(.black.opacity(0.3))
+                        )
+                        .overlay {
+                            Circle()
+                            .stroke(Color.init(white: 1, opacity: 0.5), lineWidth: 1)
+                        }
+                        .overlay(alignment: .top) {
+                            Image(systemName: "crown.fill")
+                                .font(.system(size: 8)) // クラウンアイコンのサイズを小さく
+                                .foregroundStyle(Ranking(rawValue: index)?.color() ?? .clear)
+                                .offset(y: -8) // アイコンの位置を調整
+                        }
+                }
+
+                Image(systemName: "plus")
+                    .font(.system(size: 15, weight: .bold))
+                    .foregroundStyle(.white)
+                    .padding(10)
+                    .clipShape(Circle())
+                    .background(
+                        Circle()
+                            .foregroundStyle(.black.opacity(0.3))
+                    )
+                    .overlay {
+                        Circle()
+                            .stroke(Color.init(white: 1, opacity: 0.5), lineWidth: 1)
+                    }
+            }
+            .frame(height: 35) // HStack の高さを30に設定
+        }
+    }
 }
+
+//#Preview {
+//    Top3TabView()
+//}
