14c0956038859e8c4d3b63aab6587507b4522358
Wataru Takahashi <wataru1355@icloud.com>
2024-12-05 11:43:14 +0900

    fix: 軽微な修正

--- NearU/View/Search/View/BLERealtimeView.swift
+++ NearU/View/Search/View/BLERealtimeView.swift
@@ -31,7 +31,7 @@ struct BLERealtimeView: View {
                                         isShowFollowButton: true, isShowDateButton: true)
                         } label: {
                             UserRowView(user: data.user, tags: data.tags, date: nil,
-                                        isRead: nil, rssi: data.rssi, isFollower: false)
+                                        isRead: false, rssi: data.rssi, isFollower: false)
                         }
                     } // ForEach
                 }
