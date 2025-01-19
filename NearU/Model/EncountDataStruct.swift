a223d14f0cef555af33d34f3c75047741ec2dd8c
wataru55 <wataru200155@gmail.com>
2025-01-19 15:44:35 +0900

    fix: ファイル名変更による修正

--- NearU/Model/EncountDataStruct.swift
+++ NearU/Model/EncountDataStruct.swift
@@ -11,19 +11,4 @@ struct EncountDataStruct: Hashable, Codable {
     let userId: String
     var date: Date
     var rssi: Int
-
-    init(from object: EncountData) {
-        self.userId = object.userId
-        self.date = object.date
-        self.rssi = object.rssi
-    }
-
-    // Realmオブジェクトに変換するメソッド
-    func toRealmObject() -> EncountData {
-        let realmObject = EncountData()
-        realmObject.userId = self.userId
-        realmObject.date = self.date
-        realmObject.rssi = self.rssi
-        return realmObject
-    }
 }
