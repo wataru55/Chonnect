86a2a0894d00deca1e446512cbb275d644d049d1
wataru55 <wataru200155@gmail.com>
2025-03-03 02:41:06 +0900

    fix: UIの修正

---
+++ NearU/View/Setting/View/EditPasswordView.swift
@@ -0,0 +1,18 @@
+//
+//  EditPasswordView.swift
+//  NearU
+//
+//  Created by 高橋和 on 2025/03/02.
+//
+
+import SwiftUI
+
+struct EditPasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditPasswordView()
+}
