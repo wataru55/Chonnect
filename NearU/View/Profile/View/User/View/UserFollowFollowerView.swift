c9a1f4ce3f5a661412310e94052a52310dee1891
wataru55 <wataru200155@gmail.com>
2025-05-27 18:39:33 +0900

    fix: UserFollowFollowerViewが開かれた時にデータをロードする設計に変更

--- NearU/View/Profile/View/User/View/UserFollowFollowerView.swift
+++ NearU/View/Profile/View/User/View/UserFollowFollowerView.swift
@@ -13,30 +13,41 @@ struct UserFollowFollowerView: View {
     @State var selectedTab: Int
 
     var body: some View {
-        //NavigationStack {
-            VStack {
-//                EmptyView()
-//                    .searchable(text: $searchText, prompt: "Search...")
-
-                HStack {
-                    CustomTabBarButtonView(selected: $selectedTab, title: "フォロー", tag: 0)
-                    CustomTabBarButtonView(selected: $selectedTab, title: "フォロワー", tag: 1)
-                }
-                .padding()
-
-                TabView(selection: $selectedTab) {
-                    UserFollowView(viewModel: viewModel)
-                        .tag(0)
-                    
-                    UserFollowerView(viewModel: viewModel)
-                        .tag(1)
-                }
-                .tabViewStyle(.page(indexDisplayMode: .never)) // インジケータを非表示
+        VStack {
+            HStack {
+                CustomTabBarButtonView(selected: $selectedTab, title: "フォロー", tag: 0)
+                CustomTabBarButtonView(selected: $selectedTab, title: "フォロワー", tag: 1)
             }
-            .ignoresSafeArea(edges:.bottom)
-            .navigationBarTitleDisplayMode(.inline)
-            .navigationTitle("\(viewModel.user.username)")
-        //}
+            .padding()
+            
+            TabView(selection: $selectedTab) {
+                UserFollowView(viewModel: viewModel)
+                    .tag(0)
+                
+                UserFollowerView(viewModel: viewModel)
+                    .tag(1)
+            }
+            .tabViewStyle(.page(indexDisplayMode: .never)) // インジケータを非表示
+        }
+        .ignoresSafeArea(edges:.bottom)
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationTitle("\(viewModel.user.username)")
+        .onFirstAppear {
+            viewModel.isLoading = true
+            Task {
+                await withTaskGroup(of: Void.self) { group in
+                    group.addTask {
+                        await viewModel.loadFollowUsers()
+                    }
+                    group.addTask {
+                        await viewModel.loadFollowers()
+                    }
+                }
+                await MainActor.run {
+                    viewModel.isLoading = false
+                }
+            }
+        }
     }
 }
 
