d5835786f2f13dd661f960172df1db6b116565ce
wataru55 <wataru200155@gmail.com>
2025-07-13 02:40:26 +0900

    fix: 不要なNavigationStackを消去

--- NearU/View/Profile/View/CurrentUser/View/EditSNSLinkView.swift
+++ NearU/View/Profile/View/CurrentUser/View/EditSNSLinkView.swift
@@ -19,129 +19,128 @@ struct EditSNSLinkView: View {
     ]
     
     var body: some View {
-        NavigationStack {
-            ZStack {
-                backgroundColor.ignoresSafeArea()
-                
-                ScrollView(.vertical, showsIndicators: false) {
-                    VStack(alignment: .leading, spacing: 0) {
-                        Text("SNSのURLを追加")
+        ZStack {
+            backgroundColor.ignoresSafeArea()
+            
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("SNSのURLを追加")
+                        .font(.footnote)
+                        .fontWeight(.bold)
+                        .padding(.leading, 5)
+                        .padding(.top, 10)
+                    
+                    VStack(alignment: .leading) {
+                        Text("登録可能なSNS:")
+                            .font(.caption2)
+                            .foregroundColor(.gray)
+                        
+                        Text(availableSNS.joined(separator: ", "))
+                            .font(.caption2)
+                            .foregroundColor(.gray)
+                    }
+                    .padding(.top, 3)
+                    .padding(.leading, 9)
+                    .padding(.bottom, 10)
+                    
+                    if !viewModel.isSNSLinkValid {
+                        Text("登録できないURLが含まれています")
                             .font(.footnote)
-                            .fontWeight(.bold)
-                            .padding(.leading, 5)
-                            .padding(.top, 10)
-                        
-                        VStack(alignment: .leading) {
-                            Text("登録可能なSNS:")
-                                .font(.caption2)
-                                .foregroundColor(.gray)
-                            
-                            Text(availableSNS.joined(separator: ", "))
-                                .font(.caption2)
-                                .foregroundColor(.gray)
-                        }
-                        .padding(.top, 3)
-                        .padding(.leading, 9)
-                        .padding(.bottom, 10)
-                        
-                        if !viewModel.isSNSLinkValid {
-                            Text("登録できないURLが含まれています")
-                                .font(.footnote)
-                                .foregroundStyle(.pink)
-                                .frame(maxWidth: .infinity, alignment: .center)
-                                .padding(.bottom, 5)
-                        }
-                        
-                        VStack(spacing: 3) {
-                            ForEach(viewModel.inputUrls.indices, id: \.self) { index in
-                                TextField("URLを入力", text: $viewModel.inputUrls[index])
-                                    .modifier(URLFieldModifier())
-                                    .padding(.horizontal, 10)
-                                    .background(Color(.systemGray5))
-                                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                            }
-                            
-                            Button {
-                                viewModel.inputUrls.append("")
-                            } label: {
-                                HStack {
-                                    Image(systemName: "plus.circle")
-                                        .offset(y: 3)
-                                    Text("入力欄を追加")
-                                        .padding(.top, 5)
-                                        .font(.system(size: 15, weight: .bold))
-                                }
-                                .foregroundStyle(Color.mint)
-                            }
-                            .padding(.horizontal, 15)
-                            .padding(.bottom, 10)
-                            
-                            Text("SNS一覧")
-                                .font(.footnote)
-                                .fontWeight(.bold)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .padding(.leading, 5)
-                                .padding(.vertical, 10)
-                            
-                            if viewModel.snsUrls.isEmpty {
-                                Text("SNSのリンクがありません")
-                                    .font(.subheadline)
-                                    .fontWeight(.bold)
-                                    .foregroundColor(.gray)
-                                    .padding()
-                            } else {
-                                LazyVGrid(columns: Array(repeating: .init(.flexible()), count: 3), spacing: 10) {
-                                    ForEach(Array(viewModel.snsUrls.keys), id: \.self) { key in
-                                        if let url = viewModel.snsUrls[key] {
-                                            SNSLinkButtonView(selectedSNS: key, sns_url: url, isShowDeleteButton: true)
-                                                .environmentObject(viewModel)
-                                        }
-                                    }
-                                } // LazyVGrid
-                                .padding(.horizontal, 10)
-                            }
-                        } // VStack
-                    } // VStack
-                    .padding()
-                    .navigationBarTitleDisplayMode(.inline)
-                    .navigationTitle("SNSの追加・削除")
-                }
-                .toolbar {
-                    ToolbarItem(placement: .topBarLeading) {
-                        Button {
-                            dismiss()
-                        } label: {
-                            Image(systemName: "chevron.backward")
-                                .foregroundStyle(.black)
-                        }
+                            .foregroundStyle(.pink)
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .padding(.bottom, 5)
                     }
                     
-                    ToolbarItem(placement: .topBarTrailing) {
+                    VStack(spacing: 3) {
+                        ForEach(viewModel.inputUrls.indices, id: \.self) { index in
+                            TextField("URLを入力", text: $viewModel.inputUrls[index])
+                                .modifier(URLFieldModifier())
+                                .padding(.horizontal, 10)
+                                .background(Color(.systemGray5))
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                        }
+                        
                         Button {
-                            Task {
-                                await viewModel.updateSNSLink(urls: viewModel.inputUrls)
-                            }
+                            viewModel.inputUrls.append("")
                         } label: {
-                            Text("追加")
+                            HStack {
+                                Image(systemName: "plus.circle")
+                                    .offset(y: 3)
+                                Text("入力欄を追加")
+                                    .padding(.top, 5)
+                                    .font(.system(size: 15, weight: .bold))
+                            }
+                            .foregroundStyle(Color.mint)
+                        }
+                        .padding(.horizontal, 15)
+                        .padding(.bottom, 10)
+                        
+                        Text("SNS一覧")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 5)
+                            .padding(.vertical, 10)
+                        
+                        if viewModel.snsUrls.isEmpty {
+                            Text("SNSのリンクがありません")
                                 .font(.subheadline)
                                 .fontWeight(.bold)
-                                .foregroundStyle(viewModel.isSNSLinkValid && !viewModel.isInputUrlsAllEmpty ? Color.mint : Color.gray)
+                                .foregroundColor(.gray)
+                                .padding()
+                        } else {
+                            LazyVGrid(columns: Array(repeating: .init(.flexible()), count: 3), spacing: 10) {
+                                ForEach(Array(viewModel.snsUrls.keys), id: \.self) { key in
+                                    if let url = viewModel.snsUrls[key] {
+                                        SNSLinkButtonView(selectedSNS: key, sns_url: url, isShowDeleteButton: true)
+                                            .environmentObject(viewModel)
+                                    }
+                                }
+                            } // LazyVGrid
+                            .padding(.horizontal, 10)
                         }
-                        .disabled(!viewModel.isSNSLinkValid || viewModel.isInputUrlsAllEmpty)
-                        .alert("Error", isPresented: $viewModel.isShowAlert) {
-                            Button("OK", role: .cancel) { }
-                        } message: {
-                            if let errorMessage = viewModel.errorMessage {
-                                Text(errorMessage)
-                            }
-                        }
+                    } // VStack
+                } // VStack
+                .padding()
+                .navigationBarTitleDisplayMode(.inline)
+                .navigationTitle("SNSの追加・削除")
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "chevron.backward")
+                            .foregroundStyle(.black)
                     }
                 }
                 
-                ViewStateOverlayView(state: $viewModel.state)
-            } // ZStack
-            .modifier(EdgeSwipe())
-        } // NavigationStack
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        Task {
+                            await viewModel.updateSNSLink(urls: viewModel.inputUrls)
+                        }
+                    } label: {
+                        Text("追加")
+                            .font(.subheadline)
+                            .fontWeight(.bold)
+                            .foregroundStyle(viewModel.isSNSLinkValid && !viewModel.isInputUrlsAllEmpty ? Color.mint : Color.gray)
+                    }
+                    .disabled(!viewModel.isSNSLinkValid || viewModel.isInputUrlsAllEmpty)
+                    .alert("Error", isPresented: $viewModel.isShowAlert) {
+                        Button("OK", role: .cancel) { }
+                    } message: {
+                        if let errorMessage = viewModel.errorMessage {
+                            Text(errorMessage)
+                        }
+                    }
+                }
+            }
+            
+            ViewStateOverlayView(state: $viewModel.state)
+        } // ZStack
+        .navigationBarBackButtonHidden()
+        .modifier(EdgeSwipe())
         .onDisappear {
             viewModel.inputUrls = [""]
         }
