2775893518b9f576266b551b2ddb2025c977c746
wataru55 <wataru200155@gmail.com>
2025-08-20 02:01:10 +0900

    add: キャッシュ用の設定ファイルを追加

---
+++ NearU/Config/CacheConfig.swift
@@ -0,0 +1,13 @@
+//
+//  CacheConfig.swift
+//  NearU
+//
+//  Created by 高橋和 on 2025/08/20.
+//
+
+import Foundation
+
+struct CacheConfig {
+    // ユーザープロファイルの有効期限を1時間(3600秒)に設定
+    static let userProfileTTL: TimeInterval = 3600
+}
