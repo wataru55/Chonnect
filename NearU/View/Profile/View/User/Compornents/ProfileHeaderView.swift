8eee64dbf4bf50f42280a10257f03518dc981d48
wataru55 <wataru200155@gmail.com>
2025-08-26 16:31:21 +0900

    fix: AsyncImageをKFImageに変更しCache利用

--- NearU/View/Profile/View/User/Compornents/ProfileHeaderView.swift
+++ NearU/View/Profile/View/User/Compornents/ProfileHeaderView.swift
@@ -5,6 +5,7 @@
 //  Created by  髙橋和 on 2024/05/17.
 //
 
+import Kingfisher
 import SwiftUI
 
 struct ProfileHeaderView: View {
@@ -18,43 +19,48 @@ struct ProfileHeaderView: View {
     let isShowDateButton: Bool
 
     var body: some View {
-        ZStack (alignment: .top) {
-            profileImage()
-            
+        ZStack(alignment: .top) {
+            BackgroundImageView(
+                imageUrl: viewModel.user.backgroundImageUrl,
+                height: 500,
+                isGradient: true
+            )
+
             VStack(spacing: 0) {
                 Spacer()
                     .frame(height: 50)
-                
+
                 backButton()
-                
+
                 Spacer()
                     .frame(height: 250)
-                
+
                 VStack(alignment: .leading, spacing: 6) {
-                    Attributes(attributes: viewModel.user.attributes, option: AttributeOption.profile)
-                    
+                    Attributes(
+                        attributes: viewModel.user.attributes, option: AttributeOption.profile)
+
                     userNameAndInfo()
-                    
+
                     HStack {
                         followFollowerCountView()
-                        
+
                         if isShowFollowButton {
                             Spacer()
-                            
+
                             followButton()
                         }
-                    } //HStack
+                    }  //HStack
                     .padding(.bottom, isShowFollowButton ? 0 : 5)
-                    
+
                     bio()
-                    
+
                     interestTags()
-                    
+
                     skillTags()
-                } //VStack
-            } //VStack
+                }  //VStack
+            }  //VStack
             .padding(.leading, 10)
-        } //ZStack
+        }  //ZStack
         .padding(.bottom, 10)
         .alert("確認", isPresented: $isShowCheck) {
             Button("戻る", role: .cancel) {
@@ -68,10 +74,9 @@ struct ProfileHeaderView: View {
         } message: {
             Text("本当にフォローを解除しますか？")
         }
-    }//body
-    
+    }  //body
+
     //MARK: - Helper Functions
-    
     private func backButton() -> some View {
         HStack {
             Button {
@@ -86,50 +91,11 @@ struct ProfileHeaderView: View {
                             .clipShape(Circle())
                     )
             }
-            
+
             Spacer()
         }
     }
-    
-    @ViewBuilder
-    /// プロフィール画像を表示するView
-    private func profileImage() -> some View {
-        if let backgroundImageUrl = viewModel.user.backgroundImageUrl {
-            AsyncImage(url: URL(string: backgroundImageUrl)) { image in
-                image
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: UIScreen.main.bounds.width, height: 500)
-                    .clipped()
-                    .overlay(
-                        Group{
-                            LinearGradient(
-                                gradient: Gradient(stops: [
-                                    .init(color: Color.white.opacity(0), location: 0.5),
-                                    .init(color: Color(red: 0.96, green: 0.97, blue: 0.98).opacity(1), location: 1)
-                                ]),
-                                startPoint: .top,
-                                endPoint: .bottom
-                            )
-                        }
-                    )
-            } placeholder: {
-                ProgressView()
-            }
-        } else {
-            RoundedRectangle(cornerRadius: 0)
-                .frame(width: UIScreen.main.bounds.width, height: 500)
-                .foregroundColor(Color(.systemGray4))
-                .overlay {
-                    Image(systemName: "photo.fill")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 50, height: 50)
-                        .foregroundColor(.white)
-                }
-        }
-    }
-    
+
     /// ユーザー名と情報を表示するView
     private func userNameAndInfo() -> some View {
         HStack(spacing: 4) {
@@ -137,11 +103,11 @@ struct ProfileHeaderView: View {
                 .font(.system(size: 35, weight: .bold, design: .default))
                 .padding(.bottom, 1)
                 .lineLimit(1)
-            
+
             Image(systemName: viewModel.user.isPrivate ? "lock.fill" : "lock.open.fill")
                 .font(.subheadline)
                 .offset(y: 7)
-            
+
             if isShowDateButton && !viewModel.isMyProfile {
                 SupplementButtonView(date: date, userId: viewModel.user.id)
                     .padding(.leading, 10)
@@ -150,29 +116,35 @@ struct ProfileHeaderView: View {
         }
         .offset(y: isShowFollowButton ? 5 : 0)
     }
-    
+
     /// フォローとフォロワーのカウントを表示するView
     @ViewBuilder
     private func followFollowerCountView() -> some View {
-        NavigationLink(value: ProfileDestination.FollowFollower(
-            FollowFollowerData(follows: viewModel.follows,
-                               followers: viewModel.followers,
-                               userName: viewModel.user.username,
-                               tabNum: 0)
-        )) {
+        NavigationLink(
+            value: ProfileDestination.FollowFollower(
+                FollowFollowerData(
+                    follows: viewModel.follows,
+                    followers: viewModel.followers,
+                    userName: viewModel.user.username,
+                    tabNum: 0)
+            )
+        ) {
             CountView(count: viewModel.follows.count, text: "フォロー")
         }
-        
-        NavigationLink(value: ProfileDestination.FollowFollower(
-            FollowFollowerData(follows: viewModel.follows,
-                               followers: viewModel.followers,
-                               userName: viewModel.user.username,
-                               tabNum: 1)
-        )) {
+
+        NavigationLink(
+            value: ProfileDestination.FollowFollower(
+                FollowFollowerData(
+                    follows: viewModel.follows,
+                    followers: viewModel.followers,
+                    userName: viewModel.user.username,
+                    tabNum: 1)
+            )
+        ) {
             CountView(count: viewModel.followers.count, text: "フォロワー")
         }
     }
-    
+
     /// フォローボタン
     private func followButton() -> some View {
         Group {
@@ -202,7 +174,9 @@ struct ProfileHeaderView: View {
                         .padding()
                         .frame(height: 30)
                         .background(
-                            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.mint]), startPoint: .leading, endPoint: .trailing)
+                            LinearGradient(
+                                gradient: Gradient(colors: [Color.blue, Color.mint]),
+                                startPoint: .leading, endPoint: .trailing)
                         )
                         .clipShape(Capsule())
                 }
@@ -211,7 +185,7 @@ struct ProfileHeaderView: View {
         }
         .padding(.trailing, 20)
     }
-    
+
     /// ユーザーの自己紹介を表示するView
     @ViewBuilder
     private func bio() -> some View {
@@ -226,16 +200,19 @@ struct ProfileHeaderView: View {
             .padding(.trailing, 10)
         }
     }
-    
+
     /// ユーザーの興味タグを表示するView
     @ViewBuilder
     private func interestTags() -> some View {
-        if !viewModel.user.interestTags.isEmpty{
-            InterestTagView(interestTags: viewModel.user.interestTags, isShowDeleteButton: false, textFont: .footnote)
-                .padding(.bottom, 5)
+        if !viewModel.user.interestTags.isEmpty {
+            InterestTagView(
+                interestTags: viewModel.user.interestTags, isShowDeleteButton: false,
+                textFont: .footnote
+            )
+            .padding(.bottom, 5)
         }
     }
-    
+
     /// ユーザーのスキルタグを表示するView
     @ViewBuilder
     private func skillTags() -> some View {
@@ -245,7 +222,7 @@ struct ProfileHeaderView: View {
             }
         }
     }
-}//view
+}  //view
 
 //#Preview {
 //    ProfileHeaderView(viewModel: ProfileViewModel(user: User.MOCK_USERS[1], currentUser: User.MOCK_USERS[0]),
